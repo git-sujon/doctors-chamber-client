6e8a9d264f9bd05a86cb40648ec661d7cdf29592
Mr-Robot-101 <git.sujon@gmail.com>
2022-11-13 23:05:27 +0600

    Footer Added

--- src/Pages/Shared/Footer.js
+++ src/Pages/Shared/Footer.js
@@ -1,9 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import footerBG from '../../assets/images/footer.png'
 
 const Footer = () => {
+
+    const year = new Date().getFullYear()
+
   return (
- 
+    <footer className="   mt-32" style={
+        {backgroundImage: `url(${footerBG})`,
+        backgroundSize: "cover"
+        }}>
+      <div className="footer px-24 " >
+        <div>
+          <span className="footer-title">Services</span>
+          <Link className="link link-hover">Emergency Checkup</Link>
+          <Link className="link link-hover">Monthly Checkup</Link>
+          <Link className="link link-hover">Weekly Checkup</Link>
+          <Link className="link link-hover">Deep Checkup</Link>
+        </div>
+        <div>
+          <span className="footer-title">Oral Health</span>
+          <Link className="link link-hover">Fluoride Treatment</Link>
+          <Link className="link link-hover">Cavity Filling</Link>
+          <Link className="link link-hover">Teath Whitening</Link>
+        </div>
+        <div>
+          <span className="footer-title">Our Address</span>
+          <p className="link link-hover">New York - 101010 Hudson</p>
+        </div>
+      </div>
+      <p className="text-center mt-10">Copyright {year} All Rights Reserved</p>
+    </footer>
   );
 };
 
--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
+import Appointment from "../Pages/Appointment/Appointment";
 import Home from "../Pages/Home/Home";
 
 export const router= createBrowserRouter([
@@ -12,7 +13,8 @@ export const router= createBrowserRouter([
                 element:<Home></Home>
             },
             {
-                
+                path: '/appointment',
+                element:<Appointment></Appointment>
             }
         ]
     }
